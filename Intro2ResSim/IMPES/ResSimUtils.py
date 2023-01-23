3e0276ba15ef70d94db6882b40c756a3fad1bb52
te288 <hirose65279@gmail.com>
2023-01-24 02:19:02 +0900

    bb

--- Intro2ResSim/IMPES/ResSimUtils.py
+++ Intro2ResSim/IMPES/ResSimUtils.py
@@ -1,5 +1,5 @@
 # ResSimUtils -> Reservoir Simulation Utilitiesの意味
-# ここには相対浸透率，透過率行列を返す関数を記述]
+# ここには相対浸透率，透過率行列を返す関数を記述
 import numpy as np
 from scipy.sparse import spdiags
 
--- Intro2ResSim/untitled1.py
+++
@@ -1,8 +0,0 @@
-def Keisan(a, b):
-    kaerichi = a + b
-    return kaerichi
-
-Num1 = 10;
-Num2 =  5;
-
-Goukei = Keisan(Num1, Num2);
